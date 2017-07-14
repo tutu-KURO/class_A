a373a3b506adb2f711966aafb93a76b9b88eb0de
tutu <tutugamestutubooks@gmail.com>
2017-07-14 16:09:27 +0900

    Implement redirect

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.redirect(302, '/name');//nameにリダイレクトしてくれる。
+  
 });
 
 app.get("/name", function(req, res) {
     res.send("22")
-});
 
-app.get("/show/:id",function(req, res) {
-    res.send("ID: " + req.params.id);
+});
+  //302
+
+app.get("/show/:name",function(req, res) {
+    res.send("ID: " + req.params.name);
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
